407059e6a0d8f86e89de761f915bd87705857792
Pedro Machado <pedro.machado@liferay.com>
2023-07-08 10:50:17 -0300

    feat: including common theme file

---
+++ src/common/theme/index.ts
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    blue: "#5429CC",
+    green: "#33CC95",
+    red: "#E52E4D",
+    light: "#FFFFFF",
+    title: "#363F5F",
+    text: "#969CB2",
+    background: "#F0F2F5",
+  },
+};
+
+export default theme;
