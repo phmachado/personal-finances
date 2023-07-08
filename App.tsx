fd74163ef60157f7735838f1cc23bbce92015dac
Pedro Machado <pedro.machado@liferay.com>
2023-07-08 11:33:53 -0300

    feat: including stack navigation

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from "react-native";
 
-import Home from "./src/screens/Home";
+import Routes from "./src/routes";
 
 export default function App() {
-  return <Home />;
+  return <Routes />;
 }
