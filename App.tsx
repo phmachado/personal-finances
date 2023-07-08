c2f1e0f9b42f6c97ba1d1a3cc0466c2b224deb03
Pedro Machado <pedro.machado@liferay.com>
2023-07-08 16:33:15 -0300

    feat: loading custom fonts in app

--- App.tsx
+++ App.tsx
@@ -1,7 +1,18 @@
 import React from "react-native";
+import { useFonts } from "expo-font";
 
 import Routes from "./src/routes";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    PoppinsRegular: require("./src/assets/fonts/PoppinsRegular.ttf"),
+    PoppinsMedium: require("./src/assets/fonts/PoppinsMedium.ttf"),
+    PoppinsSemiBold: require("./src/assets/fonts/PoppinsSemiBold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <Routes />;
 }
