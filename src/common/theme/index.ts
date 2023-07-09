f794f5e451c869265ef5f7640e3bd3c98cedd773
Pedro Machado <pedro.machado@liferay.com>
2023-07-09 12:31:19 -0300

    feat: including fonts to theme file

--- src/common/theme/index.ts
+++ src/common/theme/index.ts
@@ -8,6 +8,11 @@ const theme = {
     text: "#969CB2",
     background: "#F0F2F5",
   },
+  fonts: {
+    regular: "PoppinsRegular",
+    medium: "PoppinsMedium",
+    semiBold: "PoppinsSemiBold",
+  },
 };
 
 export default theme;
